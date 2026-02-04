d1c5167c8aae5bea0b0c2dd8791d57f37ae058d8
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-04 12:56:21 -0500

    refactor: corregir importaciones y comentar SideMenu en DashboardLayout

--- RickandMorty/src/shared/presentation/layouts/DashBoardLayout.tsx
+++ RickandMorty/src/shared/presentation/layouts/DashBoardLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { SideMenu, ErrorBoundary } from "../components";
-import { PageError } from "../components/erros/PageError";
+
+import { PageError } from "../handkeErrors/PageError";
+import { ErrorBoundary } from "../handkeErrors/GlobalErrorBoundary";
 
 export const DashboardLayout = () => {
   return (
@@ -11,7 +12,7 @@ export const DashboardLayout = () => {
     >
       <div className="bg-white overflow-y-scroll w-screen h-screen antialiased text-slate-900 selection:bg-blue-900 selection:text-white">
         <div className="flex flex-row relative w-screen">
-          <SideMenu />
+          {/* <SideMenu /> */}
 
           <div className="w-full p-4">
             <Outlet />
