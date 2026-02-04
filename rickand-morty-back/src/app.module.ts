42a30067337056f77ce7cdf9d12b07b68433afc4
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-03 20:15:23 -0500

    refactor: remove AppController and AppService, integrate GraphQL with Apollo Server
    - Deleted AppController and AppService as they are no longer needed.
    - Updated AppModule to configure GraphQL using ApolloDriver.
    - Added Prisma configuration and schema files for database management.

--- rickand-morty-back/src/app.module.ts
+++ rickand-morty-back/src/app.module.ts
@@ -1,10 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+      ]
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
