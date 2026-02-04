a5620c98c74d468e40371180b1f49d28826167f0
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-04 17:08:01 -0500

    feat: Add base repository interfaces and update useSelectPetitionModule for pagination support

--- RickandMorty/src/shared/presentation/hooks/use-SelectPetition-module.ts
+++ RickandMorty/src/shared/presentation/hooks/use-SelectPetition-module.ts
@@ -1,6 +1,17 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import type { BasePaginatedRepository } from "../../domain/base/base-repository.interface";
 
-
-export const UseSelectPetitionModule = () => {
-
-    return {};
+export const useSelectPetitionModule = <T>(
+    queryKey: string,
+    actions: Pick<BasePaginatedRepository<T>, 'findAll'>
+) => {
+    const { data, isLoading } = useQuery({
+        queryKey: [queryKey],
+        queryFn: () => actions.findAll(),
+        placeholderData: keepPreviousData,
+    });
+    return {
+        data,
+        isLoading,
+    };
 }
